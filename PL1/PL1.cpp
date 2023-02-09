9388735a7edd0842949a89a2c4a4e0133784f12f
Pedro Brito <pedrobfh@gmail.com>
2023-02-09 11:42:19 +0000

    Added commentaries

--- PL1/PL1.cpp
+++ PL1/PL1.cpp
@@ -2,11 +2,19 @@
 // Created by Pedro Brito on 08/02/2023.
 //
 
+// call your own library using ""
 #include "PL1.h"
+
+//directive to pre-processment(call iostream library) using <>
+//library to use the console commands
 #include <iostream>
 
 // int is the type of data to return
 int main(){
-    std::cout << "Hello World" << std::endl;
+    // cout is capable of sending the string to the console
+    //std::cout << "Hello World" << std::endl;
+
+    using namespace std;
+    cout << "Hello World 2" << endl;
     return 0;
 }

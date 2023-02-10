0e6203016ef89f9612fbc8f340ded4e135cc575f
Pedro Brito <pedrobfh@gmail.com>
2023-02-10 21:39:48 +0000

    Added test function

--- Module_1.cpp
+++ Module_1.cpp
@@ -6,7 +6,7 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+void temp() {
     int inferior = 0;
     int superior = 300;
     int passo = 20;
@@ -17,5 +17,16 @@ int main() {
         cout << "Temp_F: " << temp_f << " Temp_C: " << temp_c << endl;
         temp_f = temp_f + passo;
     }
+}
+
+void testf(int argc, char* argv[]) {
+    std::cout << argc;
+    std::cout << "O primeiro argumento do programa é : " << argv[0] << std::endl;
+    std::cout << "O segundo é :" << argv[1] << std::endl;
+    std::cout << "O terceiro é : " << argv[2] << std::endl;
+}
+
+int main() {
+    testf();
     return 0;
 }

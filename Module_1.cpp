8240a9c3710dc8bccdd375eed45672b08d8e2657
Pedro Brito <pedrobfh@gmail.com>
2023-02-10 23:09:32 +0000

    Add factorial function

--- Module_1.cpp
+++ Module_1.cpp
@@ -37,8 +37,15 @@ void testf() {
     cout << "I like " << mystr << " too!\r";
 }
 
+//factorial function
+int factorial(int n) {
+    if (n == 1)
+        return 1;
+    else
+        return n * factorial(n - 1);
+}
 
 int main() {
-    testf();
+    cout << factorial(5);
     return 0;
 }

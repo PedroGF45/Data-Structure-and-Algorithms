5661a57c41a009232058f2a00bbe34ae2dd6814f
Pedro Brito <pedrobfh@gmail.com>
2023-02-23 11:21:26 +0000

    Corrected sintax

--- Ficha_3/main.cpp
+++ Ficha_3/main.cpp
@@ -8,14 +8,14 @@ using namespace std;
 
 int main(){
     const int NUM_ELEMENTOS = 50;
-    int v[NUM_ELEMENTS];
-    cout << "Quantos elementos quer inserir no vector? (max. " << NUM_ELEMENTOS << ")" \n;
+    int v[NUM_ELEMENTOS];
+    cout << "Quantos elementos quer inserir no vector? (max. " << NUM_ELEMENTOS << ") \n";
     int tamanho;
     cin >> tamanho;
     if (tamanho > 0 && tamanho <= NUM_ELEMENTOS){
 
         bool sair = false;
-        char opcao
+        char opcao;
         do
         {
             cout << "\nEscolha uma opcao:\n";
@@ -27,31 +27,32 @@ int main(){
             cout << "0 - Sair" << endl;
             cin >> opcao;
             cout << endl;
-            switch (opcao)
+            switch (opcao) {
                 case '1':
                     cout << "Escolheu a opcao Inserir dados" << endl;
-            //inserirDados(v, tamanho);
-            case '2':
-                cout << "Escolheu a opcao Visualizar dados" << endl;
-            break;
-            case '3':
-                cout << "Escolheu a opcao Somar" << endl;
-            break;
-            case '4':
-                cout << "Escolheu a opcao Calcular media" << endl;
-            case '5':
-                cout << "Escolheu a opcao Inverter" << endl;
-            break;
-            case '0':
-                cout << "Escolheu a opcao Sair. Adeus!" << endl;
-            sair = true;
-            break;
-            default:
-                cout << "Escolha uma opcao valida." << endl;
+                    //inserirDados(v, tamanho);
+                case '2':
+                    cout << "Escolheu a opcao Visualizar dados" << endl;
+                    break;
+                case '3':
+                    cout << "Escolheu a opcao Somar" << endl;
+                    break;
+                case '4':
+                    cout << "Escolheu a opcao Calcular media" << endl;
+                case '5':
+                    cout << "Escolheu a opcao Inverter" << endl;
+                    break;
+                case '0':
+                    cout << "Escolheu a opcao Sair. Adeus!" << endl;
+                    sair = true;
+                    break;
+                default:
+                    cout << "Escolha uma opcao valida." << endl;
+            }
         } while (!sair);
     }
     else
-        cout  "Inseriu numero de elementos invalido. Prima ENTER para terminar." << endl;
-    return 0;
+        cout << "Inseriu numero de elementos invalido. Prima ENTER para terminar." << endl;
     cin.get();
+    return 0;
 }

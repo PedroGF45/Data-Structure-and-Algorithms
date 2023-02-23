51c212c5ef7f7ee986a6fe586d012e4cb5c1455b
Pedro Brito <pedrobfh@gmail.com>
2023-02-23 11:27:00 +0000

    Added functions definitions

--- Ficha_3/opVetores.h
+++ Ficha_3/opVetores.h
@@ -10,4 +10,10 @@
 //operações com vetores de inteiros
 
 void inserirDados(int v[], int tamanho);
+void escreverDados(int v[], int tamanho);
+float somarElementos(int v[], int tamanho);
+float media(int v[], int tamanho);
+void inverterVetor(int v[], int tamanho);
+int posicaoValorMax(int v[], int tamanho);
+int posicaoValorMin(int v[], int tamanho);
 

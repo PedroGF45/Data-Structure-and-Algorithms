9a8a6b88d67ed524e72be667457f10a4a2827ad6
Pedro Brito <pedrobfh@gmail.com>
2023-03-02 23:59:13 +0000

    Solved bugs for ficha 4

--- Ficha_4/2/main.cpp
+++ Ficha_4/2/main.cpp
@@ -13,22 +13,26 @@ int main() {
     const int SESSOES = 7;
 
     // allocation of the tempos matrix
-    float **tempos = new float *[ATLETAS];
-    for(int i = 0; i < ATLETAS; i++){
-        tempos[i] = new float[SESSOES];
+    float **tempos = new float *[SESSOES];
+    for(int i = 0; i < SESSOES; i++){
+        tempos[i] = new float[ATLETAS];
     }
 
-    // print choices to make
-    cout << "\nEscolha uma opcao:\n";
-    cout << "1 - Inserir os tempos dos atletas\n";
-    cout << "2 - Calcular a media dos tempos dos atletas por sessao\n";
-    cout << "3 - Melhor tempo dos atletas\n";
-    cout << "4 - Sair\n";
-
     bool sair = false;
-    char opcao;
 
     do {
+        // print choices to make
+        cout << "\nEscolha uma opcao:\n";
+        cout << "1 - Inserir os tempos dos atletas\n";
+        cout << "2 - Calcular a media dos tempos dos atletas por sessao\n";
+        cout << "3 - Melhor tempo dos atletas\n";
+        cout << "4 - Mostrar matriz\n";
+        cout << "0 - Sair\n";
+
+        char opcao;
+        cin >> opcao;
+        cout << endl;
+
         switch (opcao) {
             case '1':
                 inserirTempos(tempos, ATLETAS, SESSOES);
@@ -39,17 +43,20 @@ int main() {
             case '3':
                 escreveMinLinhas(tempos, ATLETAS, SESSOES);
                 break;
+            case '4':
+                showMatrix(tempos, ATLETAS, SESSOES);
+                break;
             case '0':
                 cout << "Adeus!" << endl;
                 sair = true;
-                break;
-                // delete allocated memory
-                for(int i = 0; i < ATLETAS;i++) {
+                //delete allocated memory
+                for (int i = 0; i < ATLETAS;i++) {
                     delete[] tempos[i];
                 }
                 delete[] tempos;
+                break;
         }
     } while (!sair);
-
+    cin.get();
     return 0;
 }

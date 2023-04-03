c84c0535f308803068990caac7fd2d9b598ce43b
Pedro Brito <pedrobfh@gmail.com>
2023-04-03 15:22:47 +0100

    Reorganized directories

---
+++ study/study.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Pedro Brito on 03/04/2023.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char * argv[]) {
+    cout << argc;
+    cout << 'o primeiro e: ' << argv[0] << endl;
+    cout << 'o segundo e: ' << argv[1] << endl;
+    cout << 'o terceiro e: ' << argv[2] << endl;
+}
